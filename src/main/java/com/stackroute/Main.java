196727782fa332fed579814647ddc0d06c34bdce
Spoorthi Samala <spoorthisamala@gmail.com>
2019-07-15 10:00:02 +0530

    Spring Annotations Task 2

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -2,8 +2,6 @@ package com.stackroute;
 
 import com.stackroute.domain.AppConfig;
 import com.stackroute.domain.Movie;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -16,13 +14,6 @@ public class Main {
         movie.actorInformation();
 
 
-        BeanFactory factory = new AnnotationConfigApplicationContext(AppConfig.class);
-        Movie movie1 = factory.getBean(Movie.class);
-        movie1.actorInformation();
-
-        BeanDefinitionRegistry beanDefinitionRegistry = new AnnotationConfigApplicationContext(AppConfig.class);
-        Movie movie2 = ((ApplicationContext)beanDefinitionRegistry).getBean(Movie.class);
-        movie2.actorInformation();
 
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -9,17 +9,13 @@ public class Movie {
     @Autowired
     Actor actor;
 
-    public Actor getActor() {
-        return actor;
-    }
-
-    public void setActor(Actor actor) {
+    public Movie(Actor actor) {
         this.actor = actor;
     }
+
     public void actorInformation(){
         actor.actorInfo();
 
     }
-
 }
 
