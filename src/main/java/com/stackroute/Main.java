47a00d2592f18cf1ee924d001f507cebb352e71b
Spoorthi Samala <spoorthisamala@gmail.com>
2019-07-15 10:23:42 +0530

    Spring Annotations Task 5

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,18 +1,23 @@
 package com.stackroute;
 
 import com.stackroute.domain.AppConfig;
+import com.stackroute.domain.BeanLifeCycleDemoBean;
 import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         Movie movie = context.getBean(Movie.class);
         movie.actorInformation();
 
+        BeanLifeCycleDemoBean beanLifeCycleDemoBean = context.getBean(BeanLifeCycleDemoBean.class);
+        context.close();
 
 
     }
--- src/main/java/com/stackroute/domain/AppConfig.java
+++ src/main/java/com/stackroute/domain/AppConfig.java
@@ -9,17 +9,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-        @Bean
-        public Movie movie(){
-            return new Movie(getActor());
-        }
+    @Bean
+    public Movie movie(){
+        return new Movie(getActor());
+    }
 
 
-        @Bean
-        public Actor getActor(){
-            Actor actor = new Actor("Bindu","Female",21);
-            return actor;
+    @Bean
+    public Actor getActor(){
+        Actor actor = new Actor("Bindu","Female",21);
+        return actor;
+    }
+
+    @Bean(initMethod="customInit", destroyMethod = "customDestroy")
+    public BeanLifeCycleDemoBean beanLifeCycleDemoBean(){
+        return new BeanLifeCycleDemoBean();
+    }
 
     }
 
-}
